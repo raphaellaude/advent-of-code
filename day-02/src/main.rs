199f5d9bf8b1af72af2849fedbe0acca9e2d5e12
Raphael Paul Laude <raphlaude@gmail.com>
2023-12-02 16:53:45 -0500

    part 2

--- day-02/src/main.rs
+++ day-02/src/main.rs
@@ -33,6 +33,14 @@ impl RGB {
 
         Ok(rgb)
     }
+
+    fn power(&self) -> u32 {
+        let red = if self.red > 0 { self.red as u32 } else { 1 };
+        let green = if self.green > 0 { self.green as u32 } else { 1 };
+        let blue = if self.blue > 0 { self.blue as u32 } else { 1 };
+
+        red * green * blue
+    }
 }
 
 impl PartialOrd for RGB {
@@ -61,26 +69,20 @@ const AVAILABLE_COLORS: RGB = RGB {
 
 fn main() {
     let input = fs::read_to_string("input.txt").unwrap();
-    let result = process(input);
 
+    let result = part_one(&input);
     println!("Total games possible {:?}", result);
 
-    let parsed = parse_game("Game 6: 4 green, 7 red, 1 blue; 18 green, 6 blue, 7 red; 1 blue, 3 red, 9 green; 9 red, 19 green, 1 blue; 7 red, 9 green, 4 blue; 5 red, 5 blue, 10 green");
-    dbg!(&parsed);
-
-    parsed.iter().for_each(|rgb| {
-        dbg!(rgb);
-        dbg!(rgb <= &AVAILABLE_COLORS);
-    });
+    let result = part_two(&input);
+    println!("Total power of games {:?}", result);
 }
 
-fn process(input: String) -> usize {
+fn part_one(input: &String) -> usize {
     input
         .lines()
         .enumerate()
         .map(|(i, line)| {
             if valid_game(line) {
-                dbg!(line);
                 return i + 1;
             }
             0
@@ -99,6 +101,43 @@ fn parse_game(game: &str) -> Vec<RGB> {
 fn valid_game(game: &str) -> bool {
     let game = parse_game(game);
     game.iter().all(|rgb| rgb <= &AVAILABLE_COLORS)
+}
+
+fn part_two(input: &String) -> u32 {
+    let games = input
+        .lines()
+        .map(|line| parse_game(line))
+        .collect::<Vec<Vec<RGB>>>();
+
+    let mut total_power = 0;
+
+    for game in games {
+        let max_color = max_color(game);
+        total_power += max_color.power();
+    }
+
+    total_power
+}
+
+fn max_color(game: Vec<RGB>) -> RGB {
+    let mut out_color = RGB::new(0, 0, 0);
+
+    for rgb in game {
+        if rgb.red > out_color.red {
+            out_color.red = rgb.red;
+        }
+
+        if rgb.green > out_color.green {
+            out_color.green = rgb.green;
+        }
+
+        if rgb.blue > out_color.blue {
+            out_color.blue = rgb.blue;
+        }
+    }
+    // dbg!(&out_color);
+
+    out_color
 }
 
 #[cfg(test)]
@@ -132,6 +171,23 @@ Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
 Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
 Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
 Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green";
-        assert_eq!(process(input.to_string()), 8)
+        assert_eq!(part_one(&input.to_string()), 8)
+    }
+
+    #[rstest]
+    #[case("Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green", 48)]
+    #[case("Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue", 12)]
+    #[case(
+        "Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red",
+        1560
+    )]
+    #[case(
+        "Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red",
+        630
+    )]
+    #[case("Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green", 36)]
+    fn test_max_color(#[case] game: &str, #[case] expected: u32) {
+        let game = parse_game(game);
+        assert_eq!(max_color(game).power(), expected);
     }
 }
