c52a98407be20b4ac6b9efb55dd81d9570104bc0
Raphael Paul Laude <raphlaude@gmail.com>
2023-12-03 11:33:55 -0500

    handle eol numbers kinda ugly

--- day-03/src/main.rs
+++ day-03/src/main.rs
@@ -16,6 +16,8 @@ fn part_one(input: &str) -> i32 {
     let mut previous_line: Option<String> = None;
 
     while let Some(line) = iter.next() {
+        let mut to_add: Vec<i32> = Vec::new();
+
         if line_len == 0 {
             line_len = line.len();
         }
@@ -42,13 +44,31 @@ fn part_one(input: &str) -> i32 {
                 };
 
                 if add_value(&line, slice_start, slice_end, &previous_line, iter.peek()) {
+                    to_add.push(*number);
+                    result += number;
+                };
+
+                number_start = None;
+            } else if (!is_num || (idx + 1 == line_len && is_num)) && number_start.is_some() {
+                let start = number_start.expect("Expected number start");
+                let number = &line[start..].parse::<i32>().unwrap();
+
+                let slice_start = if start == 0 { start } else { start - 1 };
+                let slice_end = if idx + 1 > line_len {
+                    line_len
+                } else {
+                    idx + 1
+                };
+
+                if add_value(&line, slice_start, slice_end, &previous_line, iter.peek()) {
+                    to_add.push(*number);
                     result += number;
                 };
 
                 number_start = None;
             }
         }
-
+        // println!("Adding {:?}", to_add);
         previous_line = Some(line);
     }
 
@@ -118,5 +138,21 @@ mod tests {
 ...$.*....
 .664.598..";
         assert_eq!(part_one(input), 4361);
+    }
+
+    #[test]
+    fn test_part_one_handles_eol() {
+        // added '%' below 592
+        let input = "467..114..
+...*......
+..35..633.
+......#...
+617*......
+.....+.58.
+..592.....
+...%..755.
+...$.*...$
+.664.598.1";
+        assert_eq!(part_one(input), 4362);
     }
 }
