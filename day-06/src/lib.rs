ae95ad691714c16f5393db58f02857e555b7aa98
Raphael Paul Laude <raphlaude@gmail.com>
2023-12-07 08:47:01 -0500

    day 6 part 2

--- day-06/src/lib.rs
+++ day-06/src/lib.rs
@@ -12,12 +12,33 @@ pub fn part_one(input: &str) -> u64 {
         .map(|(time, distance)| Race { time, distance })
         .collect();
 
-    dbg!(&races);
+    races
+        .iter()
+        .map(|race| num_ways_to_beat(race, race.distance))
+        .product()
+}
+
+pub fn part_two(input: &str) -> u64 {
+    let (times, distances) = parse_seqs(input);
+
+    let time = concat_nums(times);
+    let distance = concat_nums(distances);
+
+    let races: Vec<_> = vec![Race { time, distance }];
 
     races
         .iter()
         .map(|race| num_ways_to_beat(race, race.distance))
         .product()
+}
+
+fn concat_nums(nums: Vec<u64>) -> u64 {
+    let mut result = String::new();
+
+    nums.iter()
+        .for_each(|t| result += &(t.to_string().to_owned()));
+
+    result.parse::<u64>().unwrap()
 }
 
 fn parse_seqs(input: &str) -> (Vec<u64>, Vec<u64>) {
@@ -33,23 +54,13 @@ fn parse_line(start: &str, line: &str) -> Vec<u64> {
         .unwrap()
         .split(" ")
         .filter(|c| c.len() > 0)
-        .map(|s| {
-            dbg!(s);
-            s.parse::<u64>().unwrap()
-        })
+        .map(|s| s.parse::<u64>().unwrap())
         .collect::<Vec<u64>>()
 }
 
 fn num_ways_to_beat(race: &Race, time_to_beat: u64) -> u64 {
-    let iter = (0..race.time).filter(|elapsed| {
-        dbg!(elapsed * (race.time - elapsed));
-        (elapsed * (race.time - elapsed)) > time_to_beat
-    });
-
-    let (max, min) = (iter.clone().max().unwrap(), iter.min().unwrap());
-    dbg!(vec![&min, &max]);
-
-    max - min + 1
+    let iter = (0..race.time).filter(|elapsed| (elapsed * (race.time - elapsed)) > time_to_beat);
+    iter.clone().max().unwrap() - iter.min().unwrap() + 1
 }
 
 #[cfg(test)]
@@ -61,5 +72,12 @@ mod test {
         let input = "Time:      7  15   30
 Distance:  9  40  200";
         assert_eq!(part_one(&input), 288);
+    }
+
+    #[test]
+    fn test_part_two() {
+        let input = "Time:      7  15   30
+Distance:  9  40  200";
+        assert_eq!(part_two(&input), 71503);
     }
 }
--- day-06/src/main.rs
+++ day-06/src/main.rs
@@ -1,9 +1,12 @@
-use day_06::part_one;
+use day_06::{part_one, part_two};
 use std::fs;
 
 fn main() {
     let input = fs::read_to_string("input.txt").expect("Expected a valid file to read");
-    let result = part_one(&input);
 
+    let result = part_one(&input);
+    println!("Hello, world! {result}");
+
+    let result = part_two(&input);
     println!("Hello, world! {result}");
 }
