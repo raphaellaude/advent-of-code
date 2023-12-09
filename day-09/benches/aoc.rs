bac7f0d1dd00e8e23c02c7a63752870e4202f63d
Raphael Paul Laude <raphlaude@gmail.com>
2023-12-09 08:55:37 -0500

    benches

---
+++ day-09/benches/aoc.rs
@@ -0,0 +1,22 @@
+use criterion::{criterion_group, criterion_main, Criterion};
+use day_09::part_one;
+use std::fs;
+
+fn bench(c: &mut Criterion) {
+    c.bench_function("part1", |b| {
+        b.iter(|| {
+            let input = fs::read_to_string("input.txt").unwrap();
+            part_one(&input)
+        })
+    });
+
+    // c.bench_function("part2", |b| {
+    //     b.iter(|| {
+    //         let input = fs::read_to_string("input.txt").unwrap();
+    //         part_two(&input)
+    //     })
+    // });
+}
+
+criterion_group!(benches, bench);
+criterion_main!(benches);
