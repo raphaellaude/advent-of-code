5475fc1098b9085c10f040617ae50d43a55e032d
Raphael Paul Laude <raphlaude@gmail.com>
2023-12-09 15:43:58 -0500

    template for day 10

---
+++ day-10/src/lib.rs
@@ -0,0 +1,24 @@
+pub fn part_one(input: &str) -> usize {
+    todo!()
+}
+
+pub fn part_two(input: &str) -> usize {
+    todo!()
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_part_one() {
+        let input = "";
+        assert_eq!(part_one(&input), 0)
+    }
+
+    #[test]
+    fn test_part_two() {
+        let input = "";
+        assert_eq!(part_one(&input), 0)
+    }
+}
