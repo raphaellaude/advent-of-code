a10864eae03662a9ac6c28a40733b9039f40a968
Raphael Paul Laude <raphlaude@gmail.com>
2024-12-10 18:19:23 -0500

    part 1

--- 2024/day10/main.go
+++ 2024/day10/main.go
@@ -24,11 +24,97 @@ func Part1(input_file string) int {
 	check(err)
 	scanner := bufio.NewScanner(file)
 
+	starts := []Cell{}
+
+	grounds := make(map[Cell]int)
+	var x, y int
+
 	total := 0
 	for scanner.Scan() {
 		line := scanner.Text()
-		fmt.Println(line)
+
+		x = 0
+		for _, r := range line {
+			cell := Cell{x, y}
+
+			if r != '.' {
+				grounds[cell] = int(r - '0')
+			}
+
+			if r == '0' {
+				starts = append(starts, cell)
+			}
+
+			x++
+		}
+		y++
+	}
+
+	grid := Grid{rows: y, columns: x}
+
+	for _, s := range starts {
+		peaks_found := s.Climb(&grid, grounds, 9)
+		// fmt.Printf("%d: found %d peaks\n", idx, len(peaks_found))
+		total += len(peaks_found)
 	}
 
 	return total
+}
+
+type Grid struct {
+	rows    int
+	columns int
+}
+
+type Cell struct {
+	x int
+	y int
+}
+
+func (grid *Grid) IsOffMap(c *Cell) bool {
+	return c.x >= grid.columns || c.y >= grid.rows || c.x < 0 || c.y < 0
+}
+
+func (grid *Grid) RookNeighbors(c *Cell) []Cell {
+	neighbors := []Cell{}
+	poses := [][]int{{0, 1}, {0, -1}, {1, 0}, {-1, 0}}
+
+	for _, i := range poses {
+		x, y := i[0], i[1]
+		neigh := Cell{x: c.x - x, y: c.y - y}
+		if !grid.IsOffMap(&neigh) {
+			neighbors = append(neighbors, neigh)
+		}
+	}
+
+	return neighbors
+}
+
+func (cell *Cell) Climb(grid *Grid, grounds map[Cell]int, target int) map[Cell]bool {
+	peaks_found := make(map[Cell]bool)
+
+	neighbors := grid.RookNeighbors(cell)
+	c := grounds[*cell]
+
+	for _, neighbor := range neighbors {
+		t, ok := grounds[neighbor]
+
+		if !ok || t <= c {
+			continue
+		}
+
+		if t == target && c == target-1 {
+			peaks_found[neighbor] = true
+			continue
+		}
+
+		if t == c+1 {
+			new_peaks_found := neighbor.Climb(grid, grounds, target)
+			for found := range new_peaks_found {
+				peaks_found[found] = true
+			}
+		}
+	}
+
+	return peaks_found
 }
--- 2024/day10/main_test.go
+++ 2024/day10/main_test.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"fmt"
 	"testing"
 )
 
 func TestMain(t *testing.T) {
 	input_file := "./test.txt"
 	want := 36
+	fmt.Println("TEST Main")
 
 	got := Part1(input_file)
 	if got != want {
@@ -17,6 +19,7 @@ func TestMain(t *testing.T) {
 func TestMainSimple(t *testing.T) {
 	input_file := "./test_simple.txt"
 	want := 1
+	fmt.Println("TEST Simple")
 
 	got := Part1(input_file)
 	if got != want {
@@ -27,6 +30,7 @@ func TestMainSimple(t *testing.T) {
 func TestMain_02(t *testing.T) {
 	input_file := "./test_02.txt"
 	want := 2
+	fmt.Println("TEST 2")
 
 	got := Part1(input_file)
 	if got != want {
@@ -35,8 +39,9 @@ func TestMain_02(t *testing.T) {
 }
 
 func TestMain_03(t *testing.T) {
-	input_file := "./test_02.txt"
+	input_file := "./test_03.txt"
 	want := 3
+	fmt.Println("TEST 3")
 
 	got := Part1(input_file)
 	if got != want {
@@ -47,6 +52,7 @@ func TestMain_03(t *testing.T) {
 func TestMain_04(t *testing.T) {
 	input_file := "./test_04.txt"
 	want := 4
+	fmt.Println("TEST 4")
 
 	got := Part1(input_file)
 	if got != want {
